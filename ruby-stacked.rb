4250bfc559ead114e1cfd60c5885c62321ec810e
SathyaBhat <sathya@sathyasays.com>
2012-12-09 19:37:23 +0530

    Added capitalize function

--- ruby-stacked.rb
+++ ruby-stacked.rb
@@ -3,23 +3,23 @@ require 'green_shoes'
 
 Shoes.app title: 'StackEd', width: 350, height: 350 do
     def autofix
-        @edit_content.text = "blargh"
+        @edit_content.text = @edit_content.text.gsub(/\bur\b/,"your")
     end
 
-    def changecase
-        @edit_content.text = "changed"
+    def changecase(casepref)
+        if casepref == "caps"
+            @edit_content.text = @edit_content.text.capitalize
+        end
     end
     stack margin: 10 do
-    flow do
-    
+        flow do
             @autofix = button "Autofix"
-            @autofix.click { autofix }
-  
-         @autofix = button "Change case" 
-         @autofix.click { changecase }
-	end
-    stack margin: 5 do
-            @edit_content = edit_box width: 300, height: 250
+            @autofix.click { autofix }  
+            @autofix = button "Capitalize" 
+            @autofix.click { changecase("caps") }
+	    end
+        stack margin: 5 do
+                @edit_content = edit_box width: 300, height: 250
         end
     end   
 end
